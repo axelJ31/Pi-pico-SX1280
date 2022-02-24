f7c2a4e97a42a20c807979ded309c9511f97fd56
Juaneda Axel <76260529+axelJ31@users.noreply.github.com>
2022-02-24 08:40:26 +0100

    Update SX1280.cpp

--- SX1280.cpp
+++ SX1280.cpp
@@ -1,3 +1,10 @@
+/*
+  Copyright 2022 - Juaneda Axel
+  Licensed under a MIT license displayed at the bottom of this document.
+  
+  This code was adapted from the library of Stuart Robinson https://github.com/StuartsProjects/SX12XX-LoRa
+*/
+
 #include "SX1280.h"
 
 SX1280::SX1280()
@@ -700,3 +707,20 @@ uint8_t SX1280::getTXBuffer(uint8_t addr)
  return txBuffer[addr];
 }
 #endif
+
+
+
+/*
+  MIT license
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+  documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+  the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+  and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions
+  of the Software.
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+  TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+  CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+  DEALINGS IN THE SOFTWARE.
+*/
