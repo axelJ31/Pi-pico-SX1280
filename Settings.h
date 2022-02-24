d977e3807db2866e6fddc523999a0017988ac541
Juaneda Axel <76260529+axelJ31@users.noreply.github.com>
2022-02-24 08:34:28 +0100

    Update Settings.h

--- Settings.h
+++ Settings.h
@@ -1,5 +1,6 @@
 /*******************************************************************************************************
-  Programs for raspberry pi pico - Copyright of the author Stuart Robinson - 06/02/20
+  Licensed under a MIT license displayed at the bottom of this document.
+  Programs for raspberry pi pico
 
   This program is supplied as is, it is up to the user of the program to decide if the program is
   suitable for the intended purpose and free from errors.
@@ -17,6 +18,8 @@
 #define PIN_MOSI 16
 
 #define SPI_PORT spi0
+
+// Copyright of the author Stuart Robinson
 
 #define NSS 26 //26
 #define RFBUSY 2 //2
@@ -44,3 +47,17 @@
 
 #define RXBUFFER_SIZE 255                        //RX buffer size, not used in this program 
 
+/*
+  MIT license
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+  documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+  the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+  and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions
+  of the Software.
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+  TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+  CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+  DEALINGS IN THE SOFTWARE.
+*/
