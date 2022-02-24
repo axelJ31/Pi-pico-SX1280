130f9b209869089be85989e216ae0e91772867cb
Juaneda Axel <76260529+axelJ31@users.noreply.github.com>
2022-02-24 08:37:36 +0100

    Update SX1280.h

--- SX1280.h
+++ SX1280.h
@@ -1,3 +1,11 @@
+/*
+  Copyright 2022 - Juaneda Axel
+  Licensed under a MIT license displayed at the bottom of this document.
+  
+  This code was adapted from the library of Stuart Robinson https://github.com/StuartsProjects/SX12XX-LoRa
+*/
+
+
 #ifndef SX1280_H // include guard
 #define SX1280_H
 
@@ -106,3 +114,19 @@ class SX1280
 };
 
 #endif
+
+
+/*
+  MIT license
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+  documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+  the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+  and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions
+  of the Software.
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+  TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+  CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+  DEALINGS IN THE SOFTWARE.
+*/
